082f24415fbf16844ffb5189f4a1654f6b706f62
Tebogoma <tebogO@305>
2019-07-12 11:06:21 +0200

    Bugs fixes

--- src/app/components/premium-calculator/premium-calculator.component.ts
+++ src/app/components/premium-calculator/premium-calculator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { PremiumsService } from 'src/app/services/premiums.service';
 import { InsuredValueInterface } from 'src/app/models/insured-values.interface';
 import { PremiumInterface } from 'src/app/models/premim.interface';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-premium-calculator',
@@ -13,6 +14,7 @@ export class PremiumCalculatorComponent implements OnInit {
   constructor(private premiumsService: PremiumsService) { }
   insuredValues: InsuredValueInterface[];
   allInsuredValues: InsuredValueInterface[];
+  errMessage = '';
 
   @Output()
   finalPremium: EventEmitter<PremiumInterface> = new EventEmitter<PremiumInterface>();
@@ -25,16 +27,28 @@ export class PremiumCalculatorComponent implements OnInit {
   }
 
   calculatePremiums(formValues) {
-    const pidStr = formValues.pid.toString();
-// tslint:disable-next-line: radix
-    const year = pidStr.startsWith('0') ? parseInt(`20${pidStr.substring(0, 2)}`) : parseInt(`19${pidStr.substring(0, 2)}`);
-    const age = new Date().getFullYear() - year;
+    const first6Digits = formValues.pid.toString().substring(0, 6);
+    // tslint:disable-next-line: radix
+    const dateOfBirth = first6Digits.match(/.{1,2}/g).join('-');
+    if (!moment(moment(dateOfBirth).format('YY-MM-DD')).isValid()) {
+      this.errMessage = 'Cannot extract age from payer ID';
+    }
+    console.log(dateOfBirth);
+    const age = new Date().getFullYear() - new Date(moment(dateOfBirth).format('YY-MM-DD')).getFullYear();
     const obj = {
       age,
       sumIsuredId: formValues.sumInsured,
       isMember: formValues.mid.toString().length > 0
     };
-    this.premiumsService.calculatePremium(obj).subscribe((res: PremiumInterface) => this.finalPremium.emit(res));
+    this.premiumsService.calculatePremium(obj).subscribe((res: PremiumInterface) => {
+      this.finalPremium.emit(res);
+      this.errMessage = '';
+    }, (err) => this.errMessage = err.error);
+  }
+
+  filterSumValues(field) {
+    console.log(field);
+    this.insuredValues = this.allInsuredValues.filter(i => i.isMember === field.length > 0);
   }
 
 }
